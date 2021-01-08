adf06f47a23481f8de38259a54217163a8b01484
Chase Meadors <c.ed.mead@gmail.com>
2021-01-08 01:13:43 -0700

    Implement deterministic RNG using URL hash, allowing linking to particular pages.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles/nlab-theme.scss';
 
-import * as R from './random-utils.js';
+import seedrandom from 'seedrandom';
+import { RandomUtil, seedToTitle, titleToSeed } from './random-utils.js';
 import nlabGrammar from './grammar/nlab.ne';
 import NearleyGenerator from 'nearley-generator';
 
@@ -26,13 +27,32 @@ function makeTocListItem(txt, anchor) {
     return li;
 }
 
-function makePage() {
-    let g = new NearleyGenerator(nlabGrammar);
+function makePage(seed = null) {
+    let rng = null;
+    let title = null;
+    if (seed == null) {
+        // Generate a title randomly.
+        let tGenerator = new NearleyGenerator(nlabGrammar);
+        title = tGenerator.generate('title', 0.3).trim();
 
-    let t = capitalize(g.generate('title', 0.3).trim());
+        // Use that title (plus a salt) as the seed for the remainder of the page
+        seed = titleToSeed(title);
+        rng = seedrandom(seed);
 
-    document.getElementById('title').innerHTML = t;
-    document.getElementById('window-title').innerHTML = t + ' in mLab';
+        location.hash = seed;
+    } else {
+        // Find out what the title was,
+        title = seedToTitle(seed);
+        // Generate an identical page using the seed.
+        rng = seedrandom(seed);
+    }
+
+    let g = new NearleyGenerator(nlabGrammar, rng)
+    let R = new RandomUtil(rng);
+
+    let capTitle = capitalize(title);
+    document.getElementById('title').innerHTML = capTitle
+    document.getElementById('window-title').innerHTML = capTitle + ' in mLab';
     stripLinks(document.getElementById('title'));
     stripLinks(document.getElementById('window-title'));
 
@@ -108,4 +128,8 @@ function makePage() {
     }
 }
 
-makePage();
+if (location.hash.length == 0) {
+    makePage();
+} else {
+    makePage(location.hash.slice(1));
+}
--- src/random-utils.js
+++ src/random-utils.js
@@ -1,11 +1,33 @@
-let roll = probability => Math.random() < probability;
+class RandomUtil {
+    constructor(rng = null) {
+        this.rng = rng != null ? rng : Math.random.bind();
+    }
 
-function randomChoice(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+    roll(prob) {
+        return this.rng() < prob;
+    }
+
+    randomChoice(arr) {
+        return arr[Math.floor(this.rng() * arr.length)];
+    }
+    
+    randomInRange(n, m) {
+        return n + Math.floor(this.rng() * (m - n));
+    }
 }
 
-function randomInRange(n, m) {
-    return n + Math.floor(Math.random() * (m - n));
+const ALPHANUMERIC = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+function titleToSeed(title) {
+    let r = new RandomUtil();
+    let salt = [0, 1, 2, 3].map(() => r.randomChoice(ALPHANUMERIC)).join('');
+    return salt + '-' + title.replaceAll(' ', '+');
 }
 
-export { roll, randomChoice, randomInRange };
+function seedToTitle(hash) {
+    // Strip the salt off the front
+    let t = hash.slice(5);
+    return t.replaceAll('+', ' ');
+}
+
+export { RandomUtil, titleToSeed, seedToTitle };
